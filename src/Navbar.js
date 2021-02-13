059afd19cf3a8390b3cf8036ec06c667a261e59e
marina <nicolaimarina@gmail.com>
2021-02-13 17:19:42 -0500

    styling the logo and the other span tags

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,14 +7,13 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 function Navbar() {
     return (
         <div className="navbar">
-            <span><img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" srcset="" /></span>
+            <span><img className="navbar_logo" src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" srcset="" /></span>
             <span>Home</span>
             <span>TV Shows</span>
             <span>Movies</span>
             <span>Latest</span>
             <span>My List</span>
             <span>KIDS</span>
-            <span></span>
             <span><SearchIcon /></span>
             <span><NotificationsIcon /></span>
             <span><StorefrontIcon /></span>
