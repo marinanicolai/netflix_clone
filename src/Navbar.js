39fd9b7f17ca55f6d772dc278c1945fe838319e2
marina <nicolaimarina@gmail.com>
2021-02-13 09:53:09 -0500

    creating the navbar

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Navbar.css"
+import SearchIcon from '@material-ui/icons/Search';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <span><img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="" srcset="" /></span>
+            <span>Home</span>
+            <span>TV Shows</span>
+            <span>Movies</span>
+            <span>Latest</span>
+            <span>My List</span>
+            <span>KIDS</span>
+            <span></span>
+            <span><SearchIcon /></span>
+            <span><NotificationsIcon /></span>
+            <span><StorefrontIcon /></span>
+        </div>
+    );
+}
+
+export default Navbar;
