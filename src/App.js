14131e6aaba8751ce6ebe12ca2ae18ff95b55e4e
marina <nicolaimarina@gmail.com>
2021-02-14 07:13:14 -0500

    styling the banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import Banner from './Banner'
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      Netflix clone
+      <Banner />
+
     </div>
   );
 }
--- src/Banner.js
+++ src/Banner.js
@@ -6,16 +6,24 @@ import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 function Banner() {
     return (
         <div className="banner">
-            <img src="" />
+            <img src="http://i0.wp.com/www.secondhalftravels.com/wp-content/uploads/2018/08/Dark-German-series-Netflix.jpg?fit=1800,720&ssl=1" alt="" className="banner_image" />
             <h3>NETFLIX<small>ORIGINAL</small></h3>
             <h1>DARK</h1>
             <div className="button_div">
-                <div className="button1">
-                    <PlayArrowIcon style={{ fontSize: 10 }}></PlayArrowIcon>
-                </div>
-                <div className="button2">
-                    <AddIcon style={{ fontSize: 10 }}></AddIcon>
-                </div>
+                <button className="button1">
+                    <PlayArrowIcon style={{ fontSize: 10 }} />Play
+                </button>
+                <button className="button2">
+                    <AddIcon style={{ fontSize: 10 }} />My List
+                </button>
+            </div>
+            <div className="intro_container">
+                <p className="intro">
+                    The Dark Plot Summary: A serial killer brutally murders a mother and almost all
+                    of her children while her husband watches, leaving only one child alive. The
+                    raises his only remaining son, who as a result of the killer's attack is born
+                    with a physical impairment.
+                </p>
             </div>
         </div>
     )
